4ab8401a8dc1d051869acfb5bb5d5252192633ac
Masis Karapetyan <masis.karapetyan@protranslating.com>
2024-03-29 11:08:27 +0400

    Using conditional classes, rendering lists, using controlled and uncontrolled elements

--- src/App.vue
+++ src/App.vue
@@ -1,94 +1,71 @@
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+import Fruits from './components/Fruits.vue';
+
 export default {
   components: {
-    HelloWorld,
-    TheWelcome
+    Fruits
   },
   data() {
     return {
-      count: 0,
-      canShowImage: true,
+     email: '',
+     isEmailValid: false,
     };
   },
   methods: {
-    changeCounter(value) {
-      this.count += value;
+    openEmailPrompt(){
+        let email = prompt('Input your email');
+        if(!email){
+          this.isEmailValid = false;
+          return;
+        }
+        email = email.trim();
+        this.email = email;
+        console.log(email);
+        const isValid = this.checkIsEmailValid(email);
+        this.isEmailValid = isValid;
     },
-    resetCounter(){
-      this.count = 0;
+    checkIsEmailValid(email){
+      const reg = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+     return reg.test(email);
     },
-    hideImage(){
-      this.canShowImage = false;
-    },
-    showImage(){
-      this.canShowImage = true;
+    reset(){
+      this.email = '';
+      this.isEmailValid = false;
     }
   }
 }
 </script>
 
 <template>
-  <header>
-    <img v-if="canShowImage" alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <h3 v-else>The image was hidden!</h3>
-    <button @click="showImage()">Show image</button>
-<button @click="hideImage()">Hide image</button>
+<main>
+<Fruits />
 
-    <div class="wrapper">
-      <hello-world v-show="canShowImage" msg="hellooo" />
 
-      <HelloWorld msg="sfxcvcb" />
-      <HelloWorld msg="dsfsdfsssss" />
-      <TheWelcome />
-    </div>
-  </header>
+  <button @click="openEmailPrompt()">Input your email</button>
+  <button @click="reset()">Reset</button>
+<!-- <div v-if="email"> -->
+    <!-- <h1 class="green" v-if="isEmailValid">{{ email }}</h1> -->
+  <!-- <h1 class="red" v-else>{{ email }}</h1> -->
+<!-- </div> -->
 
-  <main>
-    <button @click="changeCounter(-10)">-10</button>
-    <button @click="changeCounter(-1)">-</button>
-    <span>{{ count }}</span>
-    <button @click="changeCounter(1)">+</button>
-    <button @click="changeCounter(10)">+10</button>
-    <button @click="resetCounter()">Reset</button>
-<div>The count is:
-<span v-if="count === 0">Zero</span>
-<span v-else-if="count === 1">One</span>
-<span v-else-if="count === 2">Two</span>
-<span v-else-if="count === 3">Three</span>
-<span v-else>Other</span>
 
-</div>
+  <h1 
+  :class="isEmailValid ? 'green' : 'red'"
+  v-if="email"
+  >{{ email }}</h1>
+<h4 v-if="email">{{ `The email is ${isEmailValid ? 'valid': 'invalid'}`}}</h4>
 
-  </main>
+<!-- <h1 :class="{green: isEmailValid}" class="text-center czcds dcds">Conditional class</h1> -->
+
+</main>
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
+.red{
+  border: 1px solid red;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+.green{
+  border: 1px solid green;
 }
 </style>
